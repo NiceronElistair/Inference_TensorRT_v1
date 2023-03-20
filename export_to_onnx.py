a8bafa6272d1dc7f9a618467778c00d6d4569a7a
NiceronElistair <s.niceron@elistair.com>
2023-03-20 09:52:15 +0100

    change onnx export

--- export_to_onnx.py
+++ export_to_onnx.py
@@ -4,23 +4,25 @@ import torch
 import torch.nn as nn
 from models.experimental import attempt_load
 
-def export_onnx(model, im, save_name):
+def export_onnx(model, im, save_name, opset, verbose):
     f = 'yo.onnx'
     torch.onnx.export(model,
                        im, 
                        save_name, 
                        input_names=['images'], 
                        output_names= ['output0'], 
-                       verbose=False, 
+                       verbose=verbose, 
                        dynamic_axes= False,
-                       opset_version=17, 
+                       opset_version=opset, 
                        do_constant_folding=False
                        )
     return f
 
 def run(
         weights='yolov5n.pt',
-        save_name='yolov5n.onnx'
+        save_name='yolov5n.onnx',
+        opset=13,
+        verbose=False
 ):
 
     #load model
@@ -39,12 +41,14 @@ def run(
         y = model(im)  # dry runs
 
 
-    f = export_onnx(model, im, save_name)
+    f = export_onnx(model, im, save_name, opset, verbose)
 
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, help='weights you chose to convert')
-    parser.add_argument('--save-name', type=str, help='file name for the converted weights')   
+    parser.add_argument('--save-name', type=str, help='file name for the converted weights')
+    parser.add_argument('--opset', type=int, help='opset version for the torch.onnx function')
+    parser.add_argument('verbose', action='store_true', help='verbose log')
     opt = parser.parse_args()
     return opt
 
