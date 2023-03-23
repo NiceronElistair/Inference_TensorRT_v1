674925c06f5992536cf4a3db9dfca2d2329cea68
NiceronElistair <s.niceron@elistair.com>
2023-03-23 10:20:14 +0100

    modif inference cam

--- inference_webcam.py
+++ inference_webcam.py
@@ -80,7 +80,9 @@ def run(
 
         # inference on one frame
         y = infer_one_frame(im, model, bindings, context, output_names)  # return a tensor that contain coordinates of bounding box, label probability and confience score
-        y = [y[-1]]  
+        y = [y[-1]]
+
+        print(y.shape)  
 
 
         y = non_max_suppression(y, conf_thres=0.25, iou_thres=0.45) # apply non max suppression minimize redundancy of some binding box
