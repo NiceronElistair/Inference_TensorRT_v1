a2977fcdb3f993e0b8aa9e2d8335d1b21fd368e9
NiceronElistair <s.niceron@elistair.com>
2023-03-30 09:26:23 +0200

    remove all useless files

--- tensorrt_playground.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-import tensorrt as trt
-
-f = open("resnet_engine.trt", "rb")
-runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING)) 
-
-engine = runtime.deserialize_cuda_engine(f.read())
-context = engine.create_execution_context()
